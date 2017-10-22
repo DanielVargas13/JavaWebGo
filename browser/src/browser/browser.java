96862c7f12f5535e30f20caebdebb42873e9ef62
Mcilie <rickastley404@aol.com>
2017-10-22 10:30:54 -0400

    Update browser.java

--- browser/src/browser/browser.java
+++ browser/src/browser/browser.java
@@ -1,14 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package browser;
-
-/**
- *
- * @author Tom
- */
 
 import java.awt.*;
 import java.awt.event.*;
